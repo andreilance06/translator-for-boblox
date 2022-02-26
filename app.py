009a27a9dceb7725579cf5831e4224f28845f5b9
andreilance06 <86036530+andreilance06@users.noreply.github.com>
2022-02-26 10:51:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,37 @@ from urllib.parse import unquote
 import translators as ts
 app = Flask(__name__)
 
+translated_list = {}
+converted_list = {}
+
 @app.route("/translate/")
 def trans():
-	text = unquote(request.args.get('text'))
-	data = ts.google(text, is_detail_result=True)
-	textTranslated = data[1][0][0][5][0][0]
-	lang = data[2]
-	if textTranslated:
-		return {'text': textTranslated, 'lang': lang}
+	text = unquote(request.args.get('text')).strip().lower()
+	if text in translated_list:
+		return {'text': translated_list[text]['text'], 'lang': translated_list[text]['lang']}
 	else:
-		return ''
+		data = ts.google(text, is_detail_result=True)
+		textTranslated = data[1][0][0][5][0][0] or data[1][0][0][5][0][4][0][0]
+		lang = data[2]
+		if textTranslated:
+			translated_list[text] = {'text': textTranslated, 'lang': lang}
+			return {'text': textTranslated, 'lang': lang}
+		else:
+			return ''
 
 @app.route("/translateto/")
 def transto():
-	text = unquote(request.args.get('text'))
-	lang = unquote(request.args.get('lang'))
-	if lang:
-		textTranslated = ts.google(text, 'auto', lang)
-		return {'text': textTranslated}
+	text = unquote(request.args.get('text')).strip().lower()
+	lang = unquote(request.args.get('lang')).strip().lower()
+	if '%s_%s' %(text,lang) in converted_list:
+		return converted_list['%s_%s' %(text,lang)]
 	else:
-		return ''
+		if lang:
+			textTranslated = ts.google(text, 'auto', lang)
+			converted_list['%s_%s' %(text,lang)] = textTranslated
+			return {'text': textTranslated}
+		else:
+			return ''
 
 @app.route('/')
 def index():
